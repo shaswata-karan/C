a22871c0cef8e714e11b6ed0f88d67787a848c0d
Shaswata Karan <shaswatakaran2016@gmail.com>
2023-09-20 03:33:49 +0530

    update

---
+++ 02-functions-recursion/R_HelloWorld.c
@@ -0,0 +1,19 @@
+// Print "Hello World" 3 times using recursion
+
+#include <stdio.h>
+
+void printHW(int count);
+
+int main() {
+    printHW(5);
+    return 0;
+}
+
+//recursive function
+void printHW(int count) {
+    if (count==0) {
+        return;
+    }
+    printf("Hello World\n");
+    printHW(count-1);
+}
---
+++ 02-functions-recursion/R_SumNNaturalNumbers.c
@@ -0,0 +1,22 @@
+// Print sum of N natural numbers
+
+#include <stdio.h>
+
+void Sum(int n);
+
+int main()
+{
+    Sum(n);
+    return 0;
+}
+
+// recursive function
+void Sum(int n)
+{
+    if (count == )
+    {
+        return;
+    }
+    printf("Hello World\n");
+    printHW(n - 1);
+}
